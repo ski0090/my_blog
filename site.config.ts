0b7f0682e6de8c33e925ade20a36eccae9b727d3
Shalom <ski0090@naver.com>
2023-05-11 15:35:08 +0900

    Change page id

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '5d03886956da4c688200ddf88f461b6d',
+  rootNotionPageId: '500a893f37554577b50303c0694afbf0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -25,7 +25,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fcbaa1d4b-13e7-416d-adcc-9feed1df9736%2Ffavicon.ico?table=block&id=500a893f-3755-4577-b503-03c0694afbf0&spaceId=dc4e90e3-622a-4bd6-b2ff-039b9500f5ff&userId=407c5978-77aa-417d-97c9-deb596ce1057&cache=v2',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -53,7 +53,7 @@ export default siteConfig({
   navigationLinks: [
     {
       title: '카테고리',
-      pageId: 'f0a31c6fcdf94b7580371f17af2dc9ab',
+      pageId: '044bfbb3f7a446c6a2819e103b646ea6',
     },
   ],
 
@@ -72,5 +72,5 @@ export default siteConfig({
   defaultTheme: 'system',
 
   // enable comment
-  enableComment: false,
+  enableComment: true,
 });
